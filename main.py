cec609d685ae8af6c84c5de4c9e9c6357cc8d51d
Salvador Aleguas <salvadoraleguas@gmail.com>
2019-01-06 17:25:03 -0500

    Gtk for python is a disaster. I rewrote everything to now use Qt

--- main.py
+++ main.py
@@ -1,93 +1,70 @@
-import gi
 import os
 import shutil
+import sys
+import random
 
-gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from PySide2.QtWidgets import *
+from PySide2.QtCore import Slot, Qt
 
-filepath = Gtk.Label()
 
-class MainWindow(Gtk.Window):
-		
-	global filepath
 
-	def __init__(self):
-		Gtk.Window.__init__(self, title="Desktop organizer")
-		self.set_border_width(30)
-		grid = Gtk.Grid()
-		self.add(grid)
+class MainWindow(QWidget):
 
-		dchooser = Gtk.Button("Choose directory")
-		gogo = Gtk.Button("Start")
-		filepath.set_label("No directory selected.")
-		dchooser.connect("clicked", self.on_clicked)
-		gogo.connect("clicked", self.sorting_time)
-		grid.add(dchooser)
+    filepath = ""
 
-		# grid.attach(filepath, 0, 2, 2, 1)
-		grid.attach_next_to(filepath, dchooser, Gtk.PositionType.BOTTOM, 1, 2)
-		grid.attach_next_to(gogo, filepath, Gtk.PositionType.BOTTOM, 1, 5)
-		# grid.add(dchooser)
 
+    def __init__(self):
+        QWidget.__init__(self)
+
+
+
+        self.dirbutton = QPushButton("Choose directory")
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.dirbutton)
+        self.pathlabel = QLabel()
+        self.pathlabel.setText('No directory selected')
+        self.pathlabel.setAlignment(Qt.AlignCenter)
+        self.layout.addWidget(self.pathlabel)
+        self.gobutton = QPushButton("Start")
+        self.layout.addWidget(self.gobutton)
+        self.setLayout(self.layout)
+        # Connecting the signal
+        self.dirbutton.clicked.connect(self.findDir)
+        self.gobutton.clicked.connect(self.start)
+
+
+    def findDir(self):
+        filename = QFileDialog.getExistingDirectory(None, 'Select a folder:', 'C:\\', QFileDialog.ShowDirsOnly)
+        print(filename)
+        self.pathlabel.setText(filename)
+
+
+    def start(self):
+
+        if self.pathlabel.text() != "No directory selected":
+            print(self.pathlabel.text())
+            for file in os.listdir(self.pathlabel.text()):
+                if "." in file:
+                    destination = (self.pathlabel.text() + "/" + "_" + file[file.index(".") + 1:] )
+                    source = self.pathlabel.text() + "/" + file
+                    try:
+                        os.mkdir(destination)
+                    except FileExistsError:
+                        print("it exists")
+                    print(destination)
+                    print(source)
+                    shutil.move(source, destination)
+
+app = QApplication(sys.argv)
+
+widget = MainWindow()
+widget.resize(300, 100)
+widget.show()
+
+sys.exit(app.exec_())
 		
 
 
 
 ############################################################################
-	def on_clicked(self, widget):
 
-		dialog = Gtk.FileChooserDialog("Select a directory",self,Gtk.FileChooserAction.SELECT_FOLDER,("Cancel", Gtk.ResponseType.CANCEL,"Ok", Gtk.ResponseType.OK))
-		response = dialog.run()
-		if response == Gtk.ResponseType.OK:
-			filepath.set_label(dialog.get_filename())
-		elif response == Gtk.ResponseType.CANCEL:
-			print("Nothing selected")
-
-		dialog.destroy()
-
-	def sorting_time(self, widget):
-		if filepath.get_text() != "No directory selected.":
-			print("yep")
-			for file in os.listdir(filepath.get_text()):
-				if "." in file:
-					destination = (filepath.get_text() + "/" + "_" + file[file.index(".") + 1:] )
-					source = filepath.get_text() + "/" + file
-					try:
-						os.mkdir(destination)
-					except FileExistsError:
-						print("it exists")
-					print(destination)
-					print(source)
-					shutil.move(source, destination)
-
-
-
-
-
-		else:
-			dialogg = PopUp(self)
-			response = dialogg.run()
-			if response == Gtk.ResponseType.OK:
-				print("Closed")
-
-			dialogg.destroy()
-
-
-class PopUp(Gtk.Dialog):
-
-	def __init__(self, parent):
-		Gtk.Dialog.__init__(self, "Error", parent, Gtk.DialogFlags.MODAL
-		, ("OK", Gtk.ResponseType.OK))
-
-		self.set_default_size(200, 100)
-		self.set_border_width(30)
-
-		area = self.get_content_area()
-		area.add(Gtk.Label("Please select a directory"))
-		self.show_all()
-
-
-window = MainWindow()
-window.show_all()
-window.connect("destroy", Gtk.main_quit)
-Gtk.main()
