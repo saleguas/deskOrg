a6dd9a72e51d1d94bfb899329af30a94b0cd041e
Salvador aleguas <42396136+saleguas@users.noreply.github.com>
2019-01-07 04:29:40 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ rd /s /q __pycache__
 if exist __pycache__ rd /s /q __pycache__
 rd /s /q build
 if exist build rd /s /q build
-MOVE C:\Users\drale\Documents\desktoporganizer\dist\main.exe C:\Users\drale\Documents\desktoporganizer
+MOVE %cd%\dist\main.exe %cd%
 rmdir dist /Q
 del main.spec
 pause
