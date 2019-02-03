368eb8d461ed1b1f4fcd8b3f9090041719f92fd3
Salvador Aleguas <salvadoraleguas@gmail.com>
2019-02-02 19:43:57 -0500

    i should've done this earlier

--- build.bat
+++ build.bat
@@ -1,6 +1,4 @@
-del %cd%\output\runner.exe
 del %cd%\output\dcmd.exe
-
 pyinstaller --onefile dcmd.py
 rd /s /q __pycache__
 if exist __pycache__ rd /s /q __pycache__
@@ -9,15 +7,5 @@ if exist build rd /s /q build
 MOVE %cd%\dist\dcmd.exe %cd%
 rmdir dist /Q
 del dcmd.spec
-pyinstaller --onefile runner.py
-rd /s /q build
-if exist build rd /s /q build
-MOVE %cd%\dist\runner.exe %cd%
-rmdir dist /Q
-del runner.spec
-if exist .idea rd /s /q .idea
-if exist __pycache__ rd /s /q __pycache__
-if not exist "output" mkdir "output"
-MOVE %cd%\runner.exe %cd%\output
 MOVE %cd%\dcmd.exe %cd%\output
 pause
